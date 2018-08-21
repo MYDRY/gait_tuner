4d107409e9db5aaeb976d4ca7c945f86d2b67f31
tanacchi <q111026d@mail.kyutech.jp>
2018-08-21 13:48:31 +0900

    Add time shower for each speed for debug

--- www/gait_tuner.js
+++ www/gait_tuner.js
@@ -1,7 +1,10 @@
 function judge(distance, remainingTime, defaultSpeed) {
     var walkingTime = distance / defaultSpeed.walk;
+    console.log("walkingTime: " + walkingTime);
     var joggingTime = distance / defaultSpeed.jog;
+    console.log("joggingTime: " + joggingTime);
     var runningTime = distance / defaultSpeed.run;
+    console.log("runningTime: " + runningTime);
     if (walkingTime <= remainingTime) {
         alert("歩いても間に合うよ");
     } else if (joggingTime <= remainingTime) {
