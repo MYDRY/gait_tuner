acad75f7689a5faea3ea6cb5dc641cd8093bea06
tanacchi <q111026d@mail.kyutech.jp>
2018-08-21 13:11:23 +0900

    Add getRemainingTime method on gait_tuner class

--- www/gait_tuner.js
+++ www/gait_tuner.js
@@ -16,11 +16,18 @@ GaitTuner.prototype = {
         console.log("form: " + form);
         var target_time = form.target_time.value;
         console.log(target_time);
-        return target_time;
+        tmp = target_time.split(":");
+        return new Date(2018, 7, 21, tmp[0], tmp[1]);
     },
 
     showTargetTime: function() {
         var field = document.getElementById("target_time_shower");
         field.innerHTML = this.getTargetTime();
+        this.calcRemainingTime();
+    },
+
+    calcRemainingTime: function() {
+        var ramainingTime = this.getTargetTime().getTime() - new Date().getTime();
+        console.log("Remaining time:" + (ramainingTime/1000));
     }
 };
