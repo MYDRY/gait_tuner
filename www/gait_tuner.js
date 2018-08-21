e2dd042992fbb00fe4cab7fff7f9f852bb327d3d
tanacchi <q111026d@mail.kyutech.jp>
2018-08-21 12:15:21 +0900

    Add target time setter on gait_tuner

--- www/gait_tuner.js
+++ www/gait_tuner.js
@@ -10,4 +10,17 @@ GaitTuner.prototype = {
         var date = new Date();
         return date.getHours() + ":" + date.getMinutes();
     },
+
+    getTargetTime: function() {
+        var form = document.forms['target_time_form']
+        console.log("form: " + form);
+        var target_time = form.target_time.value;
+        console.log(target_time);
+        return target_time;
+    },
+
+    showTargetTime: function() {
+        var field = document.getElementById("target_time_shower");
+        field.innerHTML = this.getTargetTime();
+    }
 };
