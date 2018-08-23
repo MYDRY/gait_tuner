5672d4fa42f7c1ea5af02541bfb6d49d90c3fb74
tanacchi <q111026d@mail.kyutech.jp>
2018-08-24 01:30:57 +0900

    Add GaitTuner class

--- www/javascript/gait_tuner.js
+++ www/javascript/gait_tuner.js
@@ -1,21 +1,25 @@
-function judge(distance, remainingTime, defaultSpeed) {
-    var walkingTime = distance / defaultSpeed.walk;
-    var joggingTime = distance / defaultSpeed.jog;
-    var runningTime = distance / defaultSpeed.run;
-
-    console.log("remainingTime: " + remainingTime);
-    console.log("walkingTime: " + walkingTime);
-    console.log("joggingTime: " + joggingTime);
-    console.log("runningTime: " + runningTime);
-
-    if (walkingTime <= remainingTime) {
-        alert("所要時間:"+ Math.round(walkingTime) +"s 歩いても間に合うよ");
-
-    } else if (joggingTime <= remainingTime) {
-        alert("ジョギングで行けば間に合うよ");
-    } else if (runningTime <= remainingTime) {
-        alert("走ったら間に合うよ");
-    } else {
-        alert("諦めろ。");
-    }
+function GaitTuner() {
 }
+
+GaitTuner.prototype = {
+    judge: function(distance, remainingTime, defaultSpeed) {
+        var walkingTime = distance / defaultSpeed.walk;
+        var joggingTime = distance / defaultSpeed.jog;
+        var runningTime = distance / defaultSpeed.run;
+
+        console.log("remainingTime: " + remainingTime);
+        console.log("walkingTime: " + walkingTime);
+        console.log("joggingTime: " + joggingTime);
+        console.log("runningTime: " + runningTime);
+
+        if (walkingTime <= remainingTime) {
+            alert("所要時間: " + Math.round(walkingTime) + "[s] 歩いても間に合うよ");
+        } else if (joggingTime <= remainingTime) {
+            alert("所要時間: " + Math.round(joggingTime) + "[s] ジョギングで行けば間に合うよ");
+        } else if (runningTime <= remainingTime) {
+            alert("所要時間: " + Math.round(runningTime) + "[s] 走ったら間に合うよ");
+        } else {
+            alert("諦めろ。");
+        }
+    }
+};
