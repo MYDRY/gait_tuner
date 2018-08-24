772093c1b456bcb6620b8e25c77b308a79fd82b9
tanacchi <q111026d@mail.kyutech.jp>
2018-08-24 16:55:12 +0900

    Add resister function on HistoryDB

--- www/javascript/history_database.js
+++ www/javascript/history_database.js
@@ -1,10 +1,10 @@
-function PlaceDB() {
+function HistoryDB() {
     this.init(name);
 };
 
-PlaceDB.prototype = {
+HistoryDB.prototype = {
     init: function() {
-        this._name = "PlaceDB";
+        this._name = "HistoryDB";
         this._db = window.openDatabase(this._name, "1.0", this._name, 100);
     },
     
@@ -12,34 +12,27 @@ PlaceDB.prototype = {
         console.warn("Error occured while executing SQL: " + err.code);
     },
     
-    getNameFromPrompt: function(tx, results) {
-        var name = prompt("この地点の名前を入力してください");
-        if (name == null) {
-            throw "No Name Error."
-        }
-        return name;
-    },
-
-    register: function(gm) {
-        alert("この地点を登録します");
-        var latlng = gm.map.getCenter();
-        console.log("lat: " + latlng.lat());
-        console.log("lng: " + latlng.lng());
+    register: function(gm, targetTime) {
+        var origin = gm.startPosition;
+        var dest   = gm.goalPosition;
         this._db.transaction(function(tx) {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS '+ this._name + ' (id unique, name, lat, lng)');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS '+ this._name + ' (origin, dest, targettime, timestamp)');
             tx.executeSql('SELECT * FROM ' + this._name, [], function(tx, results) {
-                var len = results.rows.length;
-                var name = PlaceDB.prototype.getNameFromPrompt();
-                tx.executeSql('INSERT INTO ' + this._name + ' (id, name, lat, lng) VALUES (?, ?, ?, ?)',
-                              [len + 1, name, latlng.lat(), latlng.lng()]);
+                tx.executeSql('INSERT INTO ' + this._name +
+                              ' (origin, dest, targettime, timestamp) VALUES (?, ?, ?, ?)',
+                              [origin, dest, targetTime, new Date]);
             }, this.errorCallBack);
         }, this.errorCallBack);
+        console.log(origin);
+        console.log(dest);
+        console.log(targetTime);
+        console.log(new Date());
     },
     
     show: function() {
         this._db.transaction(function(tx) {
             tx.executeSql('CREATE TABLE IF NOT EXISTS NameTable (id unique, name, lat, lng)');
-            tx.executeSql('SELECT * FROM ' + this._name, [], PlaceDB.prototype.genList, this.errorCallBack);
+            tx.executeSql('SELECT * FROM ' + this._name, [], HistoryDB.prototype.genList, this.errorCallBack);
         }, this.errorCallBack);
     },
     
@@ -58,32 +51,5 @@ PlaceDB.prototype = {
                 "==========================================================<br>";
         }
         field.innerHTML = htmlText;
-    },
-
-    hideList: function() {
-        var field = document.getElementById("placelist");
-        field.innerHTML = "<br>";
-    },
-
-    genSelectBox: function(pointName, onChangeFunc) {
-        this._db.transaction(function(tx) {
-            tx.executeSql('CREATE TABLE IF NOT EXISTS NameTable (id unique, name, lat, lng)');
-            tx.executeSql('SELECT * FROM ' + this._name, [], function(tx, results) {
-                var field = document.getElementById(pointName + "pointselector");
-                var len = results.rows.length;
-                console.log("data num: " + len);
-                var htmlText =
-                    '<div id="' + pointName + 'pointselector">' + 
-                    '<form name="' + pointName + 'pointform">' + 
-                    '<select name="' + pointName + 'point" required="true" onChange="' + onChangeFunc + '()">' +
-                    '<option value="" >選択してください</option>';
-                for (var i = 0; i < len; ++i) {
-                    var pos = results.rows.item(i).lat + "," +  results.rows.item(i).lng;
-                    htmlText += '<option value="' + pos + '">' + results.rows.item(i).name + '</option>';
-                }
-                htmlText += '</select></form></div>'
-                field.innerHTML = htmlText;
-            }, this.errorCallBack);
-        }, this.errorCallBack);        
     }
 };
