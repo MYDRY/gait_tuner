8354e1f3c553a8ea9a70c13a3122f3a581348443
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-08-24 15:54:29 +0900

    Add goal time

--- www/javascript/gait_tuner.js
+++ www/javascript/gait_tuner.js
@@ -7,17 +7,33 @@ GaitTuner.prototype = {
         var joggingTime = distance / defaultSpeed.jog;
         var runningTime = distance / defaultSpeed.run;
 
+        var sec = Math.round((walkingTime % 60) % 60);
+        var w_min = Math.floor(walkingTime/ 60) % 60;
+        var w_hour = Math.floor(walkingTime/ 3600);
+        var j_min = Math.floor(joggingTime/ 60) % 60;
+        var j_hour = Math.floor(joggingTime/ 3600);
+        var r_min = Math.floor(runningTime/ 60) % 60;
+        var r_hour = Math.floor(runningTime/ 3600);
+        var d = new Date();
+
+
         console.log("remainingTime: " + remainingTime);
         console.log("walkingTime: " + walkingTime);
         console.log("joggingTime: " + joggingTime);
         console.log("runningTime: " + runningTime);
 
         if (walkingTime <= remainingTime) {
-            alert("所要時間: " + Math.round(walkingTime) + "[s] 歩いても間に合うよ");
+            d.setHours(d.getHours()+w_hour);
+            d.setMinutes(d.getMinutes()+w_min);
+            alert("歩いても間に合うよ\n" + "到着予想時刻: "+ d.getHours() + ":" + d.getMinutes());
         } else if (joggingTime <= remainingTime) {
-            alert("所要時間: " + Math.round(joggingTime) + "[s] ジョギングで行けば間に合うよ");
+            d.setHours(d.getHours()+j_hour);
+            d.setMinutes(d.getMinutes()+j_min);
+            alert("ジョギングすれば間に合うよ\n" + "到着予想時刻: "+ d.getHours() + ":" + d.getMinutes());
         } else if (runningTime <= remainingTime) {
-            alert("所要時間: " + Math.round(runningTime) + "[s] 走ったら間に合うよ");
+            d.setHours(d.getHours()+r_hour);
+            d.setMinutes(d.getMinutes()+r_min);
+            alert("ダッシュすれば間に合うよ\n" + "到着予想時刻: "+ d.getHours() + ":" + d.getMinutes());
         } else {
             alert("諦めろ。");
         }
